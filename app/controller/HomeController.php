65ffd3e31e85114e9db0f411e985b86f4904554e
sohelrana820 <me.sohelrana@gmail.com>
2018-03-08 05:55:37 +0600

    cleanup homecontroller

--- app/controller/HomeController.php
+++ app/controller/HomeController.php
@@ -21,8 +21,6 @@ class HomeController extends AppController
      */
     public function home(Request $request, Response $response, $args)
     {
-
-        $test = $this->loadModel()->getUserModel()->test();
         $this->getFlash()->addMessage('success', 'You are ready to work!');
         $this->getLogger()->info('Application has been running successfully!');
         return $this->getView()->render($response, 'home.twig', ['message' => $this->getFlash()->getMessages()]);
