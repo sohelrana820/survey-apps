86df78c33069d2e4f5d27ca3615d33f7cf448e47
sohelrana820 <me.sohelrana@gmail.com>
2018-03-09 19:27:19 +0600

    DocBlock has been written!

--- app/controller/AuthController.php
+++ app/controller/AuthController.php
@@ -5,23 +5,51 @@ namespace App\Controller;
 use Slim\Http\Request;
 use Slim\Http\Response;
 
+/**
+ * Class AuthController
+ * @package App\Controller
+ */
 class AuthController  extends AppController
 {
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \Interop\Container\Exception\ContainerException
+     */
     public function signupPage(Request $request, Response $response, $args)
     {
         return $this->getView()->render($response, 'auth/signup.twig');
     }
 
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     */
     public function signup(Request $request, Response $response, $args)
     {
         var_dump($request->getParsedBody()); die();
     }
 
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     * @return \Psr\Http\Message\ResponseInterface
+     * @throws \Interop\Container\Exception\ContainerException
+     */
     public function loginPage(Request $request, Response $response, $args)
     {
         return $this->getView()->render($response, 'auth/login.twig');
     }
 
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @param $args
+     */
     public function login(Request $request, Response $response, $args)
     {
         var_dump($request->getParsedBody()); die();
