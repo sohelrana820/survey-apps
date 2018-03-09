2c79657cc443202c2a9c01e0705883d3d7c5bb75
sohelrana820 <me.sohelrana@gmail.com>
2018-03-09 19:10:35 +0600

    signup get and post request has been registered!

--- app/http/default.php
+++ app/http/default.php
@@ -3,4 +3,6 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 
-$app->get('/', \App\Controller\HomeController::class . ':home')->add(new \App\Middleware\AuthMiddleware());
+$app->get('/', \App\Controller\HomeController::class . ':home');
+$app->get('/signup', \App\Controller\AuthController::class . ':signupPage')->add(new \App\Middleware\AuthMiddleware());
+$app->post('/signup', \App\Controller\AuthController::class . ':signup')->add(new \App\Middleware\AuthMiddleware());
