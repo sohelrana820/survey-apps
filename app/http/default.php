084bc1f7571f79ec63469daf4052bc90cf1d7ef7
sohelrana820 <me.sohelrana@gmail.com>
2018-03-09 19:25:57 +0600

    login page and login routes has been registred routes file!

--- app/http/default.php
+++ app/http/default.php
@@ -1,8 +1,8 @@
 <?php
 
-use Slim\Http\Request;
-use Slim\Http\Response;
 
 $app->get('/', \App\Controller\HomeController::class . ':home');
-$app->get('/signup', \App\Controller\AuthController::class . ':signupPage')->add(new \App\Middleware\AuthMiddleware());
-$app->post('/signup', \App\Controller\AuthController::class . ':signup')->add(new \App\Middleware\AuthMiddleware());
+$app->get('/signup', \App\Controller\AuthController::class . ':signupPage');
+$app->post('/signup', \App\Controller\AuthController::class . ':signup');
+$app->get('/login', \App\Controller\AuthController::class . ':loginPage');
+$app->post('/login', \App\Controller\AuthController::class . ':login');
