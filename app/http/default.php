633edcb798977cafc43f07da32b98a5d0cab3669
Sohel Rana <me.sohelrana@gmail.com>
2018-03-11 19:52:39 +0600

    AuthRoutes has been removed!

--- app/http/default.php
+++ app/http/default.php
@@ -2,7 +2,6 @@
 
 
 $app->get('/', \App\Controller\HomeController::class . ':home');
-$app->get('/signup', \App\Controller\AuthController::class . ':signupPage');
-$app->post('/signup', \App\Controller\AuthController::class . ':signup');
-$app->get('/login', \App\Controller\AuthController::class . ':loginPage');
-$app->post('/login', \App\Controller\AuthController::class . ':login');
+$app->get('/404', \App\Controller\HomeController::class . ':notFoundPage');
+$app->get('/500', \App\Controller\HomeController::class . ':ServerErrorPage');
+
