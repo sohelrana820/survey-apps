e93bd79b13a62218b384ef08cd1ea4af86fd124f
sohelrana820 <me.sohelrana@gmail.com>
2018-03-13 06:37:08 +0600

    Function has been written to getting product details!

--- app/model/ProductsModel.php
+++ app/model/ProductsModel.php
@@ -13,6 +13,51 @@ use Monolog\Logger;
 class ProductsModel extends Model
 {
     /**
+     *  1 Hour cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1HOUR = 3600;
+    /**
+     *  3 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_3HOURS = 10800;
+    /**
+     *  6 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_6HOURS = 21600;
+    /**
+     *  12 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_12HOURS = 43200;
+    /**
+     *  1 Day cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1DAY = 86400;
+    /**
+     *  3 Days cache validity period in seconds
+     */
+    const CACHE_VALIDITY_3DAYS = 259200;
+    /**
+     *  5 Days cache validity period in seconds
+     */
+    const CACHE_VALIDITY_5DAYS = 432000;
+    /**
+     *  1 Week cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1WEEK = 604800;
+    /**
+     *  1 Month cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1MONTH = 2592000;
+    /**
+     * 1 Week cache validity period in seconds
+     */
+    const CACHE_VALIDITY_LONG = 604800;
+    /**
+     * 1 Month cache validity period in seconds
+     */
+    const CACHE_VALIDITY_VERY_LONG = 2592000;
+
+    /**
      * @var Logger
      */
     private $logger;
@@ -83,4 +128,34 @@ class ProductsModel extends Model
         return $this;
     }
 
+    /**
+     * @param $uuid
+     * @param bool $forceCacheGenerate
+     * @return array|null|string
+     */
+    public function getProduct($uuid, $forceCacheGenerate = false)
+    {
+        $cacheKey = sprintf('product_uuid_%s', $uuid);
+        $details = $this->cache ? $this->cache->get($cacheKey) : null;
+
+
+        if($forceCacheGenerate === false && $details) {
+            $this->logger ? $this->logger->info('Product Returned From Cache', ['product_uuid' => $uuid]) : null;
+            return $details;
+        }
+
+        try{
+            $details = $this->where('uuid', $uuid)->first();
+            if($details) {
+                $this->logger ? $this->logger->info('Product Returned From DB', ['product_uuid' => $uuid]) : null;
+                $this->cache ? $this->cache->set($cacheKey, $details->toArray(), self::CACHE_VALIDITY_1WEEK) : null;
+                return $details->toArray();
+            }
+        } catch (\Exception $exception) {
+            $this->logger ? $this->logger->error($exception->getMessage()) : null;
+            $this->logger ? $this->logger->debug($exception->getTraceAsString()) : null;
+        }
+
+        return null;
+    }
 }
