87e7c39e87051917edbd10c425395f84c0374502
sohelrana820 <me.sohelrana@gmail.com>
2018-03-19 02:49:34 +0600

    InvoicesProductsModel has been configured.

--- app/model/InvoicesProductsModel.php
+++ app/model/InvoicesProductsModel.php
@@ -13,6 +13,51 @@ use Monolog\Logger;
 class InvoicesProductsModel extends Model
 {
     /**
+     *  1 Hour cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1HOUR = 3600;
+    /**
+     *  3 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_3HOURS = 10800;
+    /**
+     *  6 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_6HOURS = 21600;
+    /**
+     *  12 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_12HOURS = 43200;
+    /**
+     *  1 Day cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1DAY = 86400;
+    /**
+     *  3 Days cache validity period in seconds
+     */
+    const CACHE_VALIDITY_3DAYS = 259200;
+    /**
+     *  5 Days cache validity period in seconds
+     */
+    const CACHE_VALIDITY_5DAYS = 432000;
+    /**
+     *  1 Week cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1WEEK = 604800;
+    /**
+     *  1 Month cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1MONTH = 2592000;
+    /**
+     * 1 Week cache validity period in seconds
+     */
+    const CACHE_VALIDITY_LONG = 604800;
+    /**
+     * 1 Month cache validity period in seconds
+     */
+    const CACHE_VALIDITY_VERY_LONG = 2592000;
+
+    /**
      * @var Logger
      */
     private $logger;
@@ -30,13 +75,20 @@ class InvoicesProductsModel extends Model
     /**
      * @var array
      */
-    protected $fillable = ['uuid'];
+    protected $fillable = ['invoice_id', 'product_id', 'name', 'unit_price', 'quantity', 'subtotal', 'created_at', 'modified_at'];
 
     /**
      * @var array
      */
     protected $casts = [
-        'uuid' => 'string',
+        'invoice_id' => 'integer',
+        'product_id' => 'integer',
+        'name' => 'string',
+        'unit_price' => 'float',
+        'quantity' => 'integer',
+        'subtotal' => 'float',
+        'created_at' => 'datetime',
+        'modified_at' => 'datetime',
     ];
 
     /**
@@ -57,5 +109,29 @@ class InvoicesProductsModel extends Model
     {
         $this->cache = $cache;
         return $this;
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function invoice()
+    {
+        return $this->belongsTo(InvoicesModel::class, 'invoice_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product()
+    {
+        return $this->belongsTo(ProductsModel::class, 'product_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function downloadLinks()
+    {
+        return $this->hasMany(DownloadLinksModel::class, 'invoices_products_id');
     }
 }
