dc7e80a6c1f2a4f8b1b2dafcc2686bc211a38dac
sohelrana820 <me.sohelrana@gmail.com>
2018-03-19 02:19:28 +0600

    Added some caching const

--- app/model/UsersModel.php
+++ app/model/UsersModel.php
@@ -13,6 +13,51 @@ use Monolog\Logger;
 class UsersModel extends Model
 {
     /**
+     *  1 Hour cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1HOUR = 3600;
+    /**
+     *  3 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_3HOURS = 10800;
+    /**
+     *  6 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_6HOURS = 21600;
+    /**
+     *  12 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_12HOURS = 43200;
+    /**
+     *  1 Day cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1DAY = 86400;
+    /**
+     *  3 Days cache validity period in seconds
+     */
+    const CACHE_VALIDITY_3DAYS = 259200;
+    /**
+     *  5 Days cache validity period in seconds
+     */
+    const CACHE_VALIDITY_5DAYS = 432000;
+    /**
+     *  1 Week cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1WEEK = 604800;
+    /**
+     *  1 Month cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1MONTH = 2592000;
+    /**
+     * 1 Week cache validity period in seconds
+     */
+    const CACHE_VALIDITY_LONG = 604800;
+    /**
+     * 1 Month cache validity period in seconds
+     */
+    const CACHE_VALIDITY_VERY_LONG = 2592000;
+
+    /**
      * @var Logger
      */
     private $logger;
