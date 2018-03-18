84346d45bfa6a5d5c9f666b3e27b059bf82cad15
sohelrana820 <me.sohelrana@gmail.com>
2018-03-19 02:49:24 +0600

    OrdersModel has been configured.

--- app/model/OrdersModel.php
+++ app/model/OrdersModel.php
@@ -13,6 +13,51 @@ use Monolog\Logger;
 class OrdersModel extends Model
 {
     /**
+     *  1 Hour cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1HOUR = 3600;
+    /**
+     *  3 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_3HOURS = 10800;
+    /**
+     *  6 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_6HOURS = 21600;
+    /**
+     *  12 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_12HOURS = 43200;
+    /**
+     *  1 Day cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1DAY = 86400;
+    /**
+     *  3 Days cache validity period in seconds
+     */
+    const CACHE_VALIDITY_3DAYS = 259200;
+    /**
+     *  5 Days cache validity period in seconds
+     */
+    const CACHE_VALIDITY_5DAYS = 432000;
+    /**
+     *  1 Week cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1WEEK = 604800;
+    /**
+     *  1 Month cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1MONTH = 2592000;
+    /**
+     * 1 Week cache validity period in seconds
+     */
+    const CACHE_VALIDITY_LONG = 604800;
+    /**
+     * 1 Month cache validity period in seconds
+     */
+    const CACHE_VALIDITY_VERY_LONG = 2592000;
+
+    /**
      * @var Logger
      */
     private $logger;
@@ -30,13 +75,23 @@ class OrdersModel extends Model
     /**
      * @var array
      */
-    protected $fillable = ['uuid'];
+    protected $fillable = ['uuid', 'user_id', 'fraud_check_status', 'fraud_check_result', 'ip_address',
+        'promo_code', 'amount', 'notes', 'created_at', 'modified_at'];
 
     /**
      * @var array
      */
     protected $casts = [
         'uuid' => 'string',
+        'user_id' => 'integer',
+        'fraud_check_status' => 'string',
+        'fraud_check_result' => 'string',
+        'ip_address' => 'string',
+        'promo_code' => 'string',
+        'amount' => 'float',
+        'notes' => 'string',
+        'created_at' => 'datetime',
+        'modified_at' => 'datetime',
     ];
 
     /**
@@ -57,5 +112,21 @@ class OrdersModel extends Model
     {
         $this->cache = $cache;
         return $this;
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(UsersModel::class, 'user_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function invoice()
+    {
+        return $this->hasMany(InvoicesModel::class, 'order_id');
     }
 }
