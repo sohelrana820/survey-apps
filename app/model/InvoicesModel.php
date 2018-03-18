280277f25104e206e90d74ef54a3813254a51761
sohelrana820 <me.sohelrana@gmail.com>
2018-03-19 02:49:44 +0600

    InvoicesModel has been configured.

--- app/model/InvoicesModel.php
+++ app/model/InvoicesModel.php
@@ -13,6 +13,51 @@ use Monolog\Logger;
 class InvoicesModel extends Model
 {
     /**
+     *  1 Hour cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1HOUR = 3600;
+    /**
+     *  3 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_3HOURS = 10800;
+    /**
+     *  6 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_6HOURS = 21600;
+    /**
+     *  12 Hours cache validity period in seconds
+     */
+    const CACHE_VALIDITY_12HOURS = 43200;
+    /**
+     *  1 Day cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1DAY = 86400;
+    /**
+     *  3 Days cache validity period in seconds
+     */
+    const CACHE_VALIDITY_3DAYS = 259200;
+    /**
+     *  5 Days cache validity period in seconds
+     */
+    const CACHE_VALIDITY_5DAYS = 432000;
+    /**
+     *  1 Week cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1WEEK = 604800;
+    /**
+     *  1 Month cache validity period in seconds
+     */
+    const CACHE_VALIDITY_1MONTH = 2592000;
+    /**
+     * 1 Week cache validity period in seconds
+     */
+    const CACHE_VALIDITY_LONG = 604800;
+    /**
+     * 1 Month cache validity period in seconds
+     */
+    const CACHE_VALIDITY_VERY_LONG = 2592000;
+
+    /**
      * @var Logger
      */
     private $logger;
@@ -30,13 +75,26 @@ class InvoicesModel extends Model
     /**
      * @var array
      */
-    protected $fillable = ['uuid'];
+    protected $fillable = ['uuid', 'order_id', 'user_id', 'subtotal', 'vat', 'tax', 'discount', 'total', 'invoice_date',
+        'due_date', 'status', 'created_at', 'modified_at'];
 
     /**
      * @var array
      */
     protected $casts = [
         'uuid' => 'string',
+        'order_id' => 'integer',
+        'user_id' => 'integer',
+        'subtotal' => 'float',
+        'vat' => 'float',
+        'tax' => 'float',
+        'discount' => 'float',
+        'total' => 'float',
+        'invoice_date' => 'datetime',
+        'due_date' => 'datetime',
+        'status' => 'string',
+        'created_at' => 'datetime',
+        'modified_at' => 'datetime',
     ];
 
     /**
@@ -57,5 +115,21 @@ class InvoicesModel extends Model
     {
         $this->cache = $cache;
         return $this;
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function order()
+    {
+        return $this->belongsTo(OrdersModel::class, 'order_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function products()
+    {
+        return $this->hasMany(InvoicesProductsModel::class, 'invoice_id');
     }
 }
