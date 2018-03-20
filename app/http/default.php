537ba1b25e27ed6a62e11a8210d382b0334c0c89
sohelrana820 <me.sohelrana@gmail.com>
2018-03-20 08:09:29 +0600

    set default timezone

--- app/http/default.php
+++ app/http/default.php
@@ -5,6 +5,7 @@ $app->get('/products', \App\Controller\ProductsController::class . ':products');
 $app->get('/products/{slug}', \App\Controller\ProductsController::class . ':productDetails');
 $app->get('/404', \App\Controller\HomeController::class . ':notFoundPage');
 $app->get('/500', \App\Controller\HomeController::class . ':ServerErrorPage');
+$app->get('/download', \App\Controller\OrdersController::class . ':download');
 
 // Routes of orders
 $app->get('/back_to', \App\Controller\OrdersController::class . ':backTo');
