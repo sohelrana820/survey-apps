0ba504ec63c4da43ee932144fdd690f80046564c
Sohel Rana <me.sohelrana@gmail.com>
2018-03-21 16:57:38 +0600

    Fixed an link!

--- app/http/default.php
+++ app/http/default.php
@@ -4,10 +4,12 @@ $app->get('/', \App\Controller\HomeController::class . ':home');
 $app->get('/products', \App\Controller\ProductsController::class . ':products');
 $app->get('/products/{slug}', \App\Controller\ProductsController::class . ':productDetails');
 $app->get('/404', \App\Controller\HomeController::class . ':notFoundPage');
-$app->get('/500', \App\Controller\HomeController::class . ':ServerErrorPage');
+$app->get('/500', \App\Controller\HomeController::class . ':serverErrorPage');
+$app->get('/403', \App\Controller\HomeController::class . ':forbiddenPage');
 $app->get('/download', \App\Controller\OrdersController::class . ':download');
 
 // Routes of orders
+$app->post('/order', \App\Controller\OrdersController::class . ':order');
 $app->get('/back_to', \App\Controller\OrdersController::class . ':backTo');
 $app->get('/email', \App\Controller\OrdersController::class . ':email');
 
