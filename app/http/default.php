7c828d5a30dbf5c4afcd394205b87504aa8e405c
sohelrana820 <me.sohelrana@gmail.com>
2018-03-22 02:38:00 +0600

    Fixed the order page!

--- app/http/default.php
+++ app/http/default.php
@@ -9,9 +9,5 @@ $app->get('/403', \App\Controller\HomeController::class . ':forbiddenPage');
 $app->get('/download', \App\Controller\OrdersController::class . ':download');
 
 // Routes of orders
-$app->post('/order', \App\Controller\OrdersController::class . ':order');
-$app->get('/order/cancel', \App\Controller\OrdersController::class . ':cancelOrder');
-$app->get('/order/confirm', \App\Controller\OrdersController::class . ':confirmOrder');
-$app->get('/back_to', \App\Controller\OrdersController::class . ':backTo');
-$app->get('/email', \App\Controller\OrdersController::class . ':email');
+$app->post('/orders', \App\Controller\OrdersController::class . ':order');
 
