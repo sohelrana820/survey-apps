42f20860b0a2efe168743f0854b7600fb464386c
sohelrana820 <me.sohelrana@gmail.com>
2018-04-09 02:22:45 +0600

    Twig Extra Extension registreed!

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -23,6 +23,7 @@ $container['view'] = function (Container $container) {
     $view = new \Slim\Views\Twig($settings['view']['template_path'], $settings['view']['twig']);
     $view->addExtension(new \Slim\Views\TwigExtension($container['router'], $container['request']->getUri()));
     $view->addExtension(new Twig_Extension_Debug());
+    $view->addExtension(new \App\components\TwigExtra());
     $twigExtra = $view->getEnvironment();
     $twigExtra->addGlobal('session', $_SESSION);
     $twigExtra->addGlobal('config', $settings);
